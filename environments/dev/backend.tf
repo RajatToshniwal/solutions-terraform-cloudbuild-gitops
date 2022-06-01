1c3cdc79536f55a4e3df558122364013f585347b
RajatToshniwal <rajat.toshniwal@gmail.com>
2022-06-01 20:36:44 +0530

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "grocery-outlet-349208-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="grocery-outlet-349208"
